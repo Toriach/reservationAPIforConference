e8f6f8a46e9ec9dded481715a8f20cf29a41a6e5
Toriach <sowa125@gmail.com>
2021-02-20 13:06:40 +0100

    init(crrs): init project

---
+++ src/main/java/com/ConferenceBookingByToriach/reservationAPI/ReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.ConferenceBookingByToriach.reservationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ConferenceBookingByToriach/reservationAPI/ReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConferenceBookingByToriach.reservationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
